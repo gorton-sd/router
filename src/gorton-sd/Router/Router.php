e40099b9b13dcc99249f3b66cbe0f8bd39c18380
Gorton Software Development <133211967+gorton-sd@users.noreply.github.com>
2024-09-19 14:27:00 +1000

    Update Router.php

--- src/gorton-sd/Router/Router.php
+++ src/gorton-sd/Router/Router.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @author      GSD <dev@gorton.com.au>
+ * @copyright   Copyright (c), 2013 Bram(us) Van Damme
+ * @license     MIT public license
+ */
+
 namespace gorton-sd\Router;
 
 class Router
